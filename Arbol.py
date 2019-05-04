579eff695ff3e526bb3461617133290cfe6b9351
Nirogu <nirogu@outlook.com>
2019-05-04 12:43:41 -0500

    Se corrigen errores en gibujar arbol y se crea el juego

--- Arbol.py
+++ Arbol.py
@@ -17,37 +17,39 @@ def Arista(Nombre,x1,y1,x2,y2):
     canvas.create_window(x2+((x1-x2)/2),y2+((y1-y2)/2),anchor = CENTER,window = Label(root,text = Nombre,bg = "white",fg="red"))
     root.update()
 
-def Arbol(num,altura,hijos,nodos,aristas):
+def Arbol(num,altura,num_hijos,nodos,aristas):
     contador = 0
     espaciado_alto = Heigh/(2*altura)
-    Node_size = (Width/hijos**altura)
+    Node_size = (Width/num_hijos**altura)
     posiciones = []
     posiciones_aristas = []
     for i in range(0,altura):
-        espaciado_ancho = Width/(2*(hijos**i))
-        espaciado_anterior = Width/(2*(hijos**(i-1)))
+        espaciado_ancho = Width/(2*(num_hijos**i))
+        espaciado_anterior = Width/(2*(num_hijos**(i-1)))
         contador_2 = 0
-        for j in range(0,hijos**i):
+        for j in range(0,num_hijos**i):
             posiciones.append([espaciado_ancho*(2*j+1),espaciado_alto*(i+1)])
             if i != 0 and i!= altura:
                 posiciones_aristas.append([espaciado_ancho*(2*j+1),espaciado_alto*(i+1),espaciado_anterior*(2*contador_2+1),espaciado_alto*(i)+Node_size])
-            if contador % hijos == 0:
+            if contador % num_hijos == 0:
                 contador_2 +=1
             contador+=1
 
     nodos_totales = 0
     Nodo(num,posiciones[0][0],posiciones[0][1],Node_size)
-    aux = nodos[num]
+    aux = []
+    for a in nodos[num]:
+        aux.append(a)
     padres = [num]
     for k in range(altura-2):
-        nodos_totales+= hijos**(k+1)
+        nodos_totales+= num_hijos**(k+1)
 
     for i in range(nodos_totales+1):
         for j in range(len(nodos[aux[0]])):
             padres.append(aux[0])
             if aux[0] !=0:
-                Nodo(aux[0],posiciones[hijos*i+j+1][0],posiciones[hijos*i+j+1][1],Node_size)
-                Arista(aristas[(padres[0],aux[0])],posiciones_aristas[hijos*i+j][0],posiciones_aristas[hijos*i+j][1],posiciones_aristas[hijos*i+j][2],posiciones_aristas[hijos*i+j][3])
+                Nodo(aux[0],posiciones[num_hijos*i+j+1][0],posiciones[num_hijos*i+j+1][1],Node_size)
+                Arista(aristas[(padres[0],aux[0])],posiciones_aristas[num_hijos*i+j][0],posiciones_aristas[num_hijos*i+j][1],posiciones_aristas[num_hijos*i+j][2],posiciones_aristas[num_hijos*i+j][3])
             for d in nodos[aux[0]]:
                 aux.append(d)
             aux.pop(0)
@@ -55,16 +57,16 @@ def Arbol(num,altura,hijos,nodos,aristas):
         if len(padres)==0:
             break
 
-init = 5
+monedas = 13
 max = 3
 last_num = max + 2
 lista_ganar = []
-while last_num < init:
+while last_num < monedas:
     lista_ganar.append(last_num)
     last_num += max+1
 lista_ganar.append(1)
 arbol = {}
-for i in range(0,init+1):
+for i in range(0,monedas+1):
     arbol[i] = [i-a for a in range(1,max+1) if i-a >= 0]
     while len(arbol[i]) < max:
         arbol[i].append(0)
@@ -74,6 +76,30 @@ for a in arbol:
     for b in arbol[a]:
         arbol_valores[(a,b)] = 1 if b in lista_ganar else 0
 
-print(arbol)
-Arbol(init,4,max,arbol,arbol_valores)
+while True:
+    jugador = input("Ingrese 1 si desea comenzar la partida. Ingrese 0 en caso contrario: ")
+    if jugador == '1' or jugador == '0':
+        jugador = bool(int(jugador))
+        break
+    else:
+        print("Por favor ingrese 1 si desea comenzar la partida, o 0 si no desea comenzar la partida.")
+
+while monedas > 1:
+    print("Ahora quedan",str(monedas),"monedas")
+    Arbol(monedas,3,max,arbol,arbol_valores)
+    if jugador:
+        eleccion = int(input("Cuantas monedas desea retirar?: "))
+        monedas -= eleccion
+    else:
+        eleccion = monedas-1
+        for hijo in arbol[monedas]:
+            if arbol_valores[(monedas,hijo)] == 1:
+                eleccion = hijo
+                break
+        print("Voy a retirar",str(monedas-eleccion),"monedas")
+        monedas = eleccion
+    jugador = not jugador
+
+if jugador: print('Queda solamente una moneda. Usted ha perdido.')
+else: print('Queda solamente una moneda. Usted ha ganado.')
 root.mainloop()
